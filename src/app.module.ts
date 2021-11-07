f5cb1828a89f707234812c5120c371b9656bdbf3
SabitTheBot <iamsabit99@gmail.com>
2021-11-07 22:18:14 +0600

    create auth controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose'
+import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/blog-api-nest')],
-  controllers: [AppController],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule { }
