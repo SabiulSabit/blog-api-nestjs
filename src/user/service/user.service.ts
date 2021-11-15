e8e26e07cee7f60d1ff395e759e2ae250608457c
SabitTheBot <iamsabit99@gmail.com>
2021-11-15 21:17:48 +0600

    create user service method added

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { from, Observable } from 'rxjs';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../models/user.entity';
+import { User } from '../models/user.interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(
+        @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>
+    ) { }
+
+    // create a user
+    creat(user: User): Observable<User> {
+        return from(this.userRepository.save(user));
+    }
+}
