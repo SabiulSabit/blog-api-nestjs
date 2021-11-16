edc31eb157133745247bc5734d00e498b969ea78
SabitTheBot <iamsabit99@gmail.com>
2021-11-16 21:47:38 +0600

    add find all routing

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -18,4 +18,10 @@ export class UserController {
     findOne(@Param() params): Observable<User> {
         return this.userService.findOne(params.id);
     }
+
+    //find all user
+    @Get()
+    findAll(): Observable<User[]> {
+        return this.userService.findAll();
+    }
 }
