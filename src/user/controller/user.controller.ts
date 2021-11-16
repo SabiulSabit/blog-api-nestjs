c57e7ef33d21064fe8113f3ce5be84f6850c4e14
SabitTheBot <iamsabit99@gmail.com>
2021-11-16 21:55:37 +0600

    update user routing added

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.interface';
 import { UserService } from '../service/user.service';
@@ -30,5 +30,12 @@ export class UserController {
     deleteOne(@Param() id: string): Observable<User> {
         return this.userService.deleteOne(Number(id));
     }
+
+    //update a user
+    @Put(':id')
+    updateOne(@Param('id') id: string, @Body() user: User): Observable<User> {
+        return this.userService.updateOne(Number(id), user);
+    }
+
 
 }
