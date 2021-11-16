7c32fec9d0386c750408d6963494fd68018f5401
SabitTheBot <iamsabit99@gmail.com>
2021-11-16 21:32:08 +0600

    create routing added

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.interface';
+import { UserService } from '../service/user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService) { }
+
+    //create a user
+    @Post()
+    create(@Body() user: User): Observable<User> {
+        return this.userService.create(user);
+    }
+}
