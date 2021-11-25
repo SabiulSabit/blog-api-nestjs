c17e051792070dac77b8bf108817dcedc6210ab8
SabitTheBot <iamsabit99@gmail.com>
2021-11-25 23:01:38 +0600

    generate jwt function added

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/user/models/user.interface';
 
 @Injectable()
 export class AuthService {
     // auth service constructor
     constructor(private readonly jwtService: JwtService) { }
+
+    //generate jwt token
+    generateJWT(user: User) {
+        return this.jwtService.signAsync({ user })
+    }
 }
