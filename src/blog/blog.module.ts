8726928ec9cf3e470336931a3d2c7e18f8342858
SabitTheBot <iamsabit99@gmail.com>
2022-01-12 22:28:33 +0600

    service added for blog

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserModule } from 'src/user/user.module';
 import { BlogController } from './blog/blog.controller';
+import { BlogService } from './blog/blog.service';
 
 @Module({
     imports: [
@@ -9,6 +10,7 @@ import { BlogController } from './blog/blog.controller';
         AuthModule,
         UserModule
     ],
-    controllers: [BlogController]
+    controllers: [BlogController],
+    providers: [BlogService]
 })
 export class BlogModule { }
