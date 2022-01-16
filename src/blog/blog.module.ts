d0beb1946fa7cfe83a6fab24faec9a9a192c59d5
SabitTheBot <iamsabit99@gmail.com>
2022-01-16 22:28:24 +0600

    blog post created

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserModule } from 'src/user/user.module';
 import { BlogController } from './controller/blog.controller';
+import { BlogSchema } from './model/blog-entry.entity';
 import { BlogService } from './service/blog.service';
 
 @Module({
     imports: [
+        MongooseModule.forFeature([{ name: 'Blog', schema: BlogSchema }]),
         // TypeOrm
         AuthModule,
-        UserModule
+        UserModule,
+        BlogModule
     ],
     controllers: [BlogController],
     providers: [BlogService]
--- src/blog/controller/blog.controller.ts
+++ src/blog/controller/blog.controller.ts
@@ -1,9 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request } from '@nestjs/common';
+import { create } from 'domain';
+import { BlogEntry } from '../model/blog-entry.interface';
 import { BlogService } from '../service/blog.service';
 
 @Controller('blog')
 export class BlogController {
     constructor(private blogService: BlogService) {
+    }
 
+    //create a post
+    @Post()
+    create(@Body() blog: BlogEntry, @Request() req) {
+        const user = req.user.user;
+        return this.blogService.create(user, blog)
     }
 }
